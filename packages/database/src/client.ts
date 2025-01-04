ac2c5e0298d524242f705c03c90a1a4d0f5feca2
NITHIN3387 <nithinn9980@gmail.com>
2025-01-04 15:06:11 +0530

    build error resolved

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from ".prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import ws from "ws";
@@ -20,4 +20,4 @@ export const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-export * from "@prisma/client";
+export * from ".prisma/client";
