e30b1c1a7479e50e10871724b5316a1f7be95b93
NITHIN3387 <nithinn9980@gmail.com>
2025-01-04 09:10:14 +0530

    prisma deployment error resolved

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -13,6 +13,7 @@ const connectionString = `${process.env.DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
+
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
